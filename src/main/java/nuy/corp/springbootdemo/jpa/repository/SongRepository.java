0d7cc3d7acd162d8eb38780bf6a923c243903cec
noelbensonuy <noel.benson.sun.uy@gmail.com>
2017-09-28 21:29:51 +0800

    pagination and tests

--- src/main/java/nuy/corp/springbootdemo/jpa/repository/SongRepository.java
+++ src/main/java/nuy/corp/springbootdemo/jpa/repository/SongRepository.java
@@ -2,6 +2,7 @@ package nuy.corp.springbootdemo.jpa.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,6 @@ import nuy.corp.springbootdemo.jpa.entity.Song;
 public interface SongRepository extends CrudRepository<Song, Long> {
 
 	@Query("select s from Song s where s.yearReleased = ?1")
-	List<Song> findByYearReleased(int yearReleased);
+	List<Song> findByYearReleased(int yearReleased, Pageable pageable);
 
 }
--- src/test/java/nuy/corp/springbootdemo/jpa/repository/SongRepositoryIntegrationTest.java
+++ src/test/java/nuy/corp/springbootdemo/jpa/repository/SongRepositoryIntegrationTest.java
@@ -8,11 +8,14 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.assertj.core.util.Lists;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -26,11 +29,14 @@ import nuy.corp.springbootdemo.jpa.entity.Song;
 @EnableAutoConfiguration
 public class SongRepositoryIntegrationTest {
 
+	Pageable page1 = new PageRequest(0, 2);
+	Pageable page2 = new PageRequest(1, 2);
+
 	@Resource
 	SongRepository songRepository;
 
 	List<Song> results;
-	Song song1, song2, song3, song4;
+	Song song1, song2, song3, song4, song5;
 
 	@Before
 	public void setup() {
@@ -38,14 +44,28 @@ public class SongRepositoryIntegrationTest {
 		song2 = createWith("Song2", 2003);
 		song3 = createWith("Song3", 1987);
 		song4 = createWith("Song4", 2017);
-		songRepository.save(Lists.newArrayList(song1, song2, song3, song4));
+		song5 = createWith("song5", 1987);
+		songRepository.save(Lists.newArrayList(song1, song2, song3, song4, song5));
+	}
+
+	@After
+	public void cleanup() {
+		songRepository.deleteAll();
 	}
 
 	@Test
 	public void findByYearReleased() {
-		results = songRepository.findByYearReleased(2003);
-
+		results = songRepository.findByYearReleased(2003, page1);
 		assertResults(results, Lists.newArrayList(song2));
+	}
+
+	@Test
+	public void findByYearReleased_byPagination() {
+		results = songRepository.findByYearReleased(1987, page1);
+		assertResults(results, Lists.newArrayList(song1, song3));
+
+		results = songRepository.findByYearReleased(1987, page2);
+		assertResults(results, Lists.newArrayList(song5));
 	}
 
 	private void assertResults(List<Song> actual, List<Song> expected) {
